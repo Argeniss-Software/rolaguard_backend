5cc70ee65a3c57e2d4e882c39d6927c8098b8242
cyones <cyones77@gmail.com>
2020-07-20 15:35:19 -0300

    Count by tag and code cleaned

--- iot_api/user_api/repository/DeviceRepository.py
+++ iot_api/user_api/repository/DeviceRepository.py
@@ -6,6 +6,7 @@ from sqlalchemy.sql import select, expression, text
 
 from iot_api.user_api import db
 from iot_api.user_api.model import Device
+from iot_api.user_api.models import DeviceToTag
 
 
 def is_from_organization(device_id, organization_id):
@@ -14,3 +15,11 @@ def is_from_organization(device_id, organization_id):
         Device.id == device_id,
         Device.organization_id == organization_id
     ).exists()).scalar()
+
+
+def query_ids_with(tag_ids):
+    return db.session.query(distinct(Device.id)).\
+            join(DeviceToTag).\
+            filter(DeviceToTag.tag_id.in_(tag_ids)).\
+            group_by(Device.id).\
+            having(func.count(DeviceToTag.tag_id) == len(tag_ids))
--- iot_api/user_api/repository/GatewayRepository.py
+++ iot_api/user_api/repository/GatewayRepository.py
@@ -6,6 +6,7 @@ from sqlalchemy.sql import select, expression, text
 
 from iot_api.user_api import db
 from iot_api.user_api.model import Gateway
+from iot_api.user_api.models import GatewayToTag, DeviceToTag
 
 
 def is_from_organization(gateway_id, organization_id):
@@ -14,3 +15,11 @@ def is_from_organization(gateway_id, organization_id):
         Gateway.id == gateway_id,
         Gateway.organization_id == organization_id
     ).exists()).scalar()
+
+
+def query_ids_with(tag_ids):
+    return db.session.query(distinct(Gateway.id)).\
+            join(GatewayToTag).\
+            filter(GatewayToTag.tag_id.in_(tag_ids)).\
+            group_by(Gateway.id).\
+            having(func.count(GatewayToTag.tag_id) == len(tag_ids))
