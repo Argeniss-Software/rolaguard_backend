c40e183b1bc948e5e0e3d53f7c09de316147f7ac
cyones <cristianyones@argeniss.com>
2020-08-03 14:41:36 -0300

    Added exception type generated when parsing args.

--- iot_api/user_api/Error.py
+++ iot_api/user_api/Error.py
@@ -1,6 +1,7 @@
 import traceback
 from iot_api import app
 import werkzeug
+import marshmallow
 
 import iot_logging
 log = iot_logging.getLogger(__name__)
@@ -68,6 +69,8 @@ def handle_error(error):
         return handle_404(NotFound(error))
     elif isinstance(error, werkzeug.exceptions.UnprocessableEntity):
         return handle_422(UnprocessableEntity(error))
+    elif isinstance(error, marshmallow.ValidationError):
+        return handle_400(BadRequest(error))
     else:
         # For not typified exceptions, the server respond with a html code 500 
         # and save the message and traceback in the log.
