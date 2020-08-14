164644fa55a2fbaede5c49b416744d48bad566d8
lsantire <lsantire2@gmail.com>
2020-08-14 18:24:24 -0300

    Added more data about packets on response

--- iot_api/user_api/resources/ResourceUsage.py
+++ iot_api/user_api/resources/ResourceUsage.py
@@ -5,6 +5,8 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 import iot_logging, calendar
 log = iot_logging.getLogger(__name__)
 
+from collections import namedtuple
+
 from iot_api.user_api.model import User
 from iot_api.user_api.Utils import is_system
 from iot_api.user_api.repository import ResourceUsageRepository
@@ -40,6 +42,15 @@ class ResourceUsageListAPI(Resource):
             asset_type=request.args.get('asset_type', type=str)
         )
 
+        PacketsInfo = namedtuple('PacketsInfo', ['up', 'down', 'lost'])
+        packet_counts = [PacketsInfo(
+            dev.npackets_up,
+            dev.npackets_down,
+            int(round(dev.packet_loss * dev.npackets_up)) if dev.packet_loss is not None else 0,
+        ) for dev in results.items]
+
+        uptimes = [dev.npackets_up * dev.activity_freq if dev.activity_freq else 0 for dev in results.items]
+
         assets = [{
             'id': dev.id,
             'hex_id': dev.hex_id,
@@ -50,14 +61,32 @@ class ResourceUsageListAPI(Resource):
             'connected': dev.connected,
             'last_activity': calendar.timegm(dev.last_activity.timetuple()),
             'activity_freq': dev.activity_freq,
-            'npackets_up': dev.npackets_up,
-            'npackets_down': dev.npackets_down,
-            'packet_loss': dev.packet_loss,
+            'packets_up': buildPacketsInfo(uptime, packets.up, sum(list(packets))),
+            'packets_down': buildPacketsInfo(uptime, packets.down, sum(list(packets))),
+            'packets_lost': buildPacketsInfo(uptime, packets.lost, sum(list(packets))) if dev.packet_loss is not None else None,
             'max_rssi': dev.max_rssi
-        } for dev in results.items]
+        } for dev, packets, uptime in zip(results.items, packet_counts, uptimes)]
         response = {
             'assets': assets,
             'total_pages': results.pages,
             'total_items': results.total
         }
         return response, 200
+
+def buildPacketsInfo(uptime, count, total):
+    """ Helper function to calculate data related with packets received from an asset
+    Request parameters (all required):
+        - uptime: time since asset is connected in seconds
+        - count: number of packets of the type (up, down, lost) to calculate data for
+        - total: number of packets in total
+    Returns:
+        - JSON with packets related information (see code for more details about the fields).
+    """
+    secs_bw_pakcets = uptime/count if count else None
+    return {
+        'total': count if secs_bw_pakcets else 0,
+        'per_minute': 60/secs_bw_pakcets if secs_bw_pakcets else 0,
+        'per_hour': 60*60/secs_bw_pakcets if secs_bw_pakcets else 0,
+        'per_day': 24*60*60/secs_bw_pakcets if secs_bw_pakcets else 0,
+        'percentage': 100*count/total if total else None
+    }
