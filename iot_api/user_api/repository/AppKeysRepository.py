bf329daa215596fb48c42ca8a97363e234879edf
lsantire <lsantire2@gmail.com>
2020-08-19 14:31:47 -0300

    Added POST method for AppKeysAPI

--- iot_api/user_api/repository/AppKeysRepository.py
+++ iot_api/user_api/repository/AppKeysRepository.py
@@ -5,10 +5,46 @@ from iot_api.user_api import db
 from iot_api.user_api.models import AppKey
 from iot_api.user_api import Error
 
+MAX_PER_ORGANIZATION = 500
 
-def list_all(organization_id):
+def count_with(organization_id):
+    """ Count all app keys of an organization """
+    return db.session.query(AppKey).filter(AppKey.organization_id==organization_id).count()
+
+def get_with(organization_id, keys_list=None):
+    """ List app keys of an organization.
+    Parameters:
+        - organization_id: which organization,
+        - keys_list: for filtering, list only app keys that are present in this list
     """
-    List all app keys of an organization.
-    """
-    result = db.session.query(AppKey).filter(AppKey.organization_id==organization_id).all()
+    qry = db.session.query(AppKey).filter(AppKey.organization_id==organization_id)
+    if keys_list:
+        qry = qry.filter(AppKey.key.in_(keys_list))
+    result = qry.all()
     return result if result else []
+
+def create(keys_list, organization_id): 
+    """
+    Create a new app_key for every key in keys_list that is
+    not already part of this organizantion's set of keys.
+    """
+    global MAX_PER_ORGANIZATION
+
+    already_in_db = set(row.key for row in get_with(
+        organization_id = organization_id,
+        keys_list = keys_list))
+    total = count_with(organization_id = organization_id)
+    created = 0
+
+    for key in keys_list:
+        if key not in already_in_db:
+            db.session.add(AppKey(key = key, organization_id = organization_id))
+            already_in_db.add(key)
+            created = created + 1
+
+    if total + created > MAX_PER_ORGANIZATION:
+        db.session.rollback()
+        raise Error.Forbidden("Creating these app keys would exceed the limit per organization")
+
+    db.session.commit()
+    return created
--- iot_api/user_api/resources/AppKeys.py
+++ iot_api/user_api/resources/AppKeys.py
@@ -1,18 +1,25 @@
 from flask import request
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required, get_jwt_identity
-import json
+import json, string
 
 from iot_api.user_api.model import User
 from iot_api.user_api.Utils import is_system
 from iot_api.user_api.repository import AppKeysRepository
+from iot_api.user_api.schemas.app_keys_schema import AppKeysSchema
 from iot_api.user_api import Error
 
+from iot_api.user_api.repository.AppKeysRepository import MAX_PER_ORGANIZATION
+
+import iot_logging
+log = iot_logging.getLogger(__name__)
 
 class AppKeysAPI(Resource):
     """
     Resource to list all app keys (GET), create new ones (with POST) and
     delete some of the existing ones (with DELETE).
+    For POST and DELETE, the body must be a non-emtpy list of keys, where
+    every key contains only hex digits.
     """
     @jwt_required
     def get(self):
@@ -21,7 +28,7 @@ class AppKeysAPI(Resource):
             raise Error.Forbidden("User not allowed")
         organization_id = user.organization_id
 
-        app_keys = AppKeysRepository.list_all(organization_id = organization_id)
+        app_keys = AppKeysRepository.get_with(organization_id = organization_id)
 
         return [{
             "id": app_key.id,
@@ -29,3 +36,38 @@ class AppKeysAPI(Resource):
             "organization_id": app_key.organization_id
         } for app_key in app_keys], 200
         
+    @jwt_required
+    def post(self):
+        user = User.find_by_username(get_jwt_identity())
+        if not user or is_system(user.id):
+            raise Error.Forbidden("User not allowed")
+        organization_id = user.organization_id
+        
+        body = json.loads(request.data)
+        parsed_result = AppKeysSchema().load(body).data
+
+        app_keys = parsed_result.get('keys')
+        if app_keys is None:
+            raise Error.BadRequest(f"AppKeysAPI POST request body must contain a non-empty list of keys with at most {MAX_PER_ORGANIZATION} keys")
+
+        hex_digits = set(list(string.hexdigits))
+
+        total = len(app_keys)
+
+        # Remove duplicates and validate app keys
+        app_keys = list(set(app_keys))
+        log.debug(f"app_keys {app_keys}")
+        for key in app_keys:
+            if len(key) != 32:
+                raise Error.BadRequest(f"Every key must have 32 characters, but received one with length {len(key)}")
+            for char in key:
+                if char not in hex_digits:
+                    raise Error.BadRequest(f"Every key must contain only hex digits, but one had a \"{char}\"")
+
+        not_duplicated = len(app_keys)
+
+        created = AppKeysRepository.create(
+            keys_list = app_keys,
+            organization_id = organization_id)
+        
+        return {"message": f"{created} app keys created, {total-not_duplicated} were duplicated and {not_duplicated-created} already existed"}, 200
