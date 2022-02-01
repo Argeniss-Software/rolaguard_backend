54d304eeacc13de5706302b5db35b83250908b58
JU4N98 <juanpablocabana2@gmail.com>
2022-02-01 09:35:13 -0300

    Adds webhook-secret cipher.

--- iot_api/user_api/resources/NotificationPreferences.py
+++ iot_api/user_api/resources/NotificationPreferences.py
@@ -13,6 +13,7 @@ from threading import Thread
 from datetime import datetime, timedelta
 from urllib.parse import quote_plus
 
+from iot_api import cipher_suite
 from iot_api import bcrypt, mail, app
 from iot_api.user_api.model import User, Webhook
 #from iot_api.user_api.enums import WebUrl
@@ -179,7 +180,8 @@ class NotificationPreferencesAPI(Resource):
 
                     for webhook in destination.get('additional'):
                         if webhook and not webhook.get('id'):
-                            Webhook(webhook_user_id=user.id,target_url=webhook.get('url'),url_secret=webhook.get('secret'),active=True).save()
+                            encrypted_url_secret = cipher_suite.encrypt(bytes(webhook.get('secret'),'utf-8')).decode('utf-8')
+                            Webhook(webhook_user_id=user.id,target_url=webhook.get('url'),url_secret=encrypted_url_secret,active=True).save()
 
             # Update emails -> Delete removed, add new as pending, change to pending to updated
             # Update phones ->Delete removed, add new as pending, change to pending to updated
